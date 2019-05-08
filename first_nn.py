465c86049cca7f6138bf44ddc56b876f97073004
Cookize <38367063+Cookize@users.noreply.github.com>
2019-05-08 23:31:55 +0800

    Update first_nn.py

--- first_nn.py
+++ first_nn.py
@@ -1,13 +1,44 @@
-import math
 import torch
-import numpy as np
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)
+        self.predict = torch.nn.Linear(n_hidden, n_output)
+
+    def forward(self, x):
+        x = F.relu(self.hidden(x))
+        x = self.predict(x)
+        return x
 
 x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim = 1)
 y = x.pow(2) + 0.2 * torch.rand(x.size())
 
-plt.scatter(x.data.numpy(), y.data.numpy())
+net = Net(1, 10, 1)
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.2)
+
+loss_func = torch.nn.MSELoss()
+
+plt.ion()
+
+for t in range(200):
+    prediction = net(x)
+    loss = loss_func(prediction, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if t % 5 == 0:
+        plt.clf()
+        plt.scatter(x.numpy(), y.numpy())
+        plt.scatter(x.numpy(), y.numpy())
+        plt.plot(x.numpy(), prediction.data.numpy(), 'r-', lw=5)
+        plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
+        plt.pause(0.1)
+
+plt.ioff()
 plt.show()
-
-
 
