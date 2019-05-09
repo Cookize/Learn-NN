8161c1249bccad29fa2169538060c34c73230561
Cookize <38367063+Cookize@users.noreply.github.com>
2019-05-09 14:53:04 +0800

    完成分类NN

---
+++ Basic_nn/classification_nn.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
+        self.out = torch.nn.Linear(n_hidden, n_output)   # output layer
+
+    def forward(self, x):
+        x = F.relu(self.hidden(x))      # activation function for hidden layer
+        x = self.out(x)
+        return x
+
+
+n_data = torch.ones(100, 2)         # 数据的基本形态
+x0 = torch.normal(2*n_data, 1)      # 类型0 x data (tensor), shape=(100, 2)
+y0 = torch.zeros(100)               # 类型0 y data (tensor), shape=(100, )
+x1 = torch.normal(-2*n_data, 1)     # 类型1 x data (tensor), shape=(100, 1)
+y1 = torch.ones(100)                # 类型1 y data (tensor), shape=(100, )
+x = torch.cat((x0, x1), 0).float() # FloatTensor = 32-bit floating
+y = torch.cat((y0, y1), ).long()    # LongTensor = 64-bit integer
+
+# plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=y.data.numpy(), s=100, lw=0, cmap='RdYlGn')
+# plt.show()
+
+# 画图
+# plt.scatter(x.numpy(), y.numpy())
+# plt.show()
+
+net = Net(2, 10, 2)
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.02)
+loss_func = torch.nn.CrossEntropyLoss()  # the target label is NOT an one-hotted
+
+plt.ion()   # something about plotting
+
+for t in range(100):
+    out = net(x)                 # input x and predict based on x
+    loss = loss_func(out, y)     # must be (1. nn output, 2. target), the target label is NOT one-hotted
+
+    optimizer.zero_grad()   # clear gradients for next train
+    loss.backward()         # backpropagation, compute gradients
+    optimizer.step()        # apply gradients
+
+    if t % 2 == 0:
+        # plot and show learning process
+        plt.clf()
+        prediction = torch.max(out, 1)[1]
+        pred_y = prediction.numpy()
+        target_y = y.numpy()
+        plt.scatter(x.numpy()[:, 0], x.numpy()[:, 1], c=pred_y, s=100, lw=0, cmap='RdYlGn')
+        accuracy = float((pred_y == target_y).astype(int).sum()) / float(target_y.size)
+        plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color':  'red'})
+        plt.pause(0.1)
+
+plt.ioff()
+plt.show()
+print("exit")
